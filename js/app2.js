d7ca426639c5e080a42e2073d00cd91ed6ecd326
JudyVue <judyvue87@gmail.com>
2016-08-08 16:21:13 -0700

    judy's working on review page HTML and JS in app2.js

--- js/app2.js
+++ js/app2.js
@@ -9,3 +9,28 @@ for (var i = 0; i < accordion.length; i++) {
     this.nextElementSibling.classList.toggle('show');
   };
 }
+
+
+//judy's code for retaining user's answers in collapsed answer review in accordion menu
+// 1.Use local storage as answers in array with yes and no's as strings
+// 2. stringify to local storage
+// 3. use JSON to parse Array
+// 4. access array at their respective elements
+// 5. after parse, match respective index with appropriate radio button
+
+
+//need for loop to grab yes's and no's
+
+var yesAndNoArray = [];
+
+function grabYesAndNos() {
+  var answers = document.getElementsByName('input.radio_buttons');
+  var test = answers.includes('Yes');
+  console.log(test);
+  yesAndNoArray.push(answers);
+  console.log(answers);
+}
+
+grabYesAndNos();
+
+// localStorage.setItem('userResultsForLocalStorage', JSON.stringify())
