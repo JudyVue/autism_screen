602bb86155e0aa25779725b201ea5eb1dffdd51d
Maelle Vance <maellevance@gmail.com>
2016-08-09 10:38:15 -0700

    create login links to instructions page driver Maelle navigator Rachael

--- js/app-data.js
+++ js/app-data.js
@@ -17,6 +17,7 @@ function handleNewLogin() {
   loginArrayStringified = JSON.stringify(loginArray);
   localStorage.setItem('loginArrayStringified', loginArrayStringified);
   console.log(loginArrayStringified);
+  location.href = 'instructions.html';
 
 }
 
