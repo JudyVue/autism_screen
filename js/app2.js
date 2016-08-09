eede23aaa760f1ed65ed16ee9da461f677c57cbd
JudyVue <judyvue87@gmail.com>
2016-08-09 11:35:22 -0700

    judy's committing

--- js/app2.js
+++ js/app2.js
@@ -3,12 +3,15 @@
 
 var accordion = document.getElementsByClassName('accordion');
 
-for (var i = 0; i < accordion.length; i++) {
-  accordion[i].onclick = function(){
-    this.classList.toggle('active');
-    this.nextElementSibling.classList.toggle('show');
-  };
+function makeAccordion(){
+  for (var i = 0; i < accordion.length; i++) {
+    accordion[i].onclick = function(){
+      this.classList.toggle('active');
+      this.nextElementSibling.classList.toggle('show');
+    };
+  }
 }
+makeAccordion();
 
 //judy's code for retaining user's answers in collapsed answer review in accordion menu
 // 1.Use local storage as answers in array with yes and no's as strings
@@ -20,20 +23,17 @@ for (var i = 0; i < accordion.length; i++) {
 
 //need for loop to grab yes's and no's
 
-// var checked;
-var yesAndNoArray = [];
+
+// var yesAndNoArray = [];
 var no = document.getElementsByName('No');
 
 
 function grabYesAndNos(){
-for (var j = 0; j < userAnswers.length; j++) {
-  if (userAnswers[j] === 'score') {
-    console.log(no);
+  for (var j = 0; j < userAnswers.length; j++) {
+    if (userAnswers[j] === 'score') {
+      no.checked = true;
+    }
   }
 }
 
-function check() {
-  document.getElementsByTagName('input').checked = true;
-}
-
-// localStorage.setItem('userResultsForLocalStorage', JSON.stringify())
+grabYesAndNos();
