5e4edb2e36280c9bd885bcdfba5a3dfe6c7e1886
JudyVue <judyvue87@gmail.com>
2016-08-09 15:19:01 -0700

    judy added buttons to test_page.html

--- js/app2.js
+++ js/app2.js
@@ -1,5 +1,5 @@
 'use strict';
-//judy's JS code for accordion menu
+//judy's JS code for accordion menu with contributions from Maelle and Rachael
 
 var accordion = document.getElementsByClassName('accordion');
 
@@ -18,29 +18,29 @@ makeAccordion();
 var reviewData = [];
 var noArray;
 var yesArray;
-function populateReviewPage(){
+
+//parses the data
+function parseData(){
   reviewData = JSON.parse(localStorage.userAnswersStringified);
-  console.log(reviewData);
 }
+parseData();
 
-populateReviewPage();
-
+//grabs the name = 'No' value of the input from HTML
 function pushNosIntoArray() {
   var no = document.getElementsByName('No');
   noArray = Array.prototype.slice.call(no);
 }
 
+//grabs the name = 'Yes' value of the input from HTML
 function pushYesesIntoArray() {
   var yes = document.getElementsByName('Yes');
   yesArray = Array.prototype.slice.call(yes);
 }
 
-
-console.log(noArray);
-console.log(yesArray);
 pushYesesIntoArray();
 pushNosIntoArray();
 
+//retains the user's selections from the previous questions as they review
 function grabYesAndNos(){
   for (var j = 0; j < reviewData.length; j++) {
     if (reviewData[j] === 'score') {
@@ -51,3 +51,24 @@ function grabYesAndNos(){
   }
 }
 grabYesAndNos();
+
+//code to hide and display test question divs on test_page.html
+// var question_set1 = document.getElementById('question_set1');
+// var question_set2 = document.getElementById('question_set2');
+// var question_set3 = document.getElementById('question_set3');
+// var question_set4 = document.getElementById('question_set4');
+// var question_set5 = document.getElementById('question_set5');
+
+var questionset = document.getElementsByClassName('questionset');
+var next = document.getElementsByClassName('next');
+
+
+//TRYING TO FIGURE OUT HOW TO MAKE THIS SHIT DRY
+function clickNextButton() {
+  for (var k = 0; k < questionset.length; ++k) {
+    var s = questionset[i].style;
+    s.display = s.display === 'none' ? 'block' : 'none';
+  }
+}
+
+next.addEventListener('click', clickNextButton);
