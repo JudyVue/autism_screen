9928da9a6780c1e11e3f735a4fba204cfd9158eb
Maelle Vance <maellevance@gmail.com>
2016-08-09 11:33:28 -0700

    create login works team effort

--- js/app-data.js
+++ js/app-data.js
@@ -2,12 +2,27 @@
 var loginForm = document.getElementById('loginForm');
 var loginArray = [];
 var loginArrayStringified;
+var storedData = [];
 
 
 function handleNewLogin() {
   event.preventDefault();
   var username = event.target.username.value;
   var password = event.target.password.value;
+  //username already exists logic:
+  if (localStorage.loginArrayStringified) {
+    storedData = JSON.parse(localStorage.loginArrayStringified);
+    for (var i = 0; i < storedData.length; i++) {
+      if (storedData[i].username === username) {
+        console.log(storedData[i].username);
+        alert('username already exists');
+        event.target.username.value = null;
+        event.target.password.value = null;
+        return;
+      }
+    }
+  }
+
   function Login(username, password) {
     this.username = username;
     this.password = password;
@@ -18,7 +33,8 @@ function handleNewLogin() {
   localStorage.setItem('loginArrayStringified', loginArrayStringified);
   console.log(loginArrayStringified);
   location.href = 'instructions.html';
-
 }
+
+
 
 loginForm.addEventListener('submit', handleNewLogin);
